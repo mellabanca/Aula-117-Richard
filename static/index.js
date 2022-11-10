9d602d2c5dde27055d2bfff0a7063b5f75594b16
mellabanca <91677889+mellabanca@users.noreply.github.com>
2022-11-10 16:17:04 -0300

    10/11/2022

---
+++ static/index.js
@@ -0,0 +1,38 @@
+//Crie a variável de data
+var date = new Date();
+var display_date = "Data: " + date.toLocaleDateString("pt-BR",{
+                                                                weekday: "short",
+                                                                year: "numeric",
+                                                                month: "short",
+                                                                day:"numeric"});
+
+//Carregue o DOM HTML
+
+//Defina a variável para armazenar a emoção prevista
+
+
+//HTML-->JavaScript--->Flask
+//Flask--->JavaScript--->HTML
+
+//seletor jQuery e ação de clique
+
+$(function () {
+    $("#predict_button").click(function () {
+        //chamada AJAX
+
+        $.ajax({
+            
+            
+              {
+                
+                // Resultado recebido do Flask ----->JavaScript
+                
+                // Exibir resultado usando JavaScript----->HTML
+                
+            },
+            //Função de erro
+            
+        });
+    });
+})
+
